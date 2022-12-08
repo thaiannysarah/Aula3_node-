15c4599afaa484b799e28f09d2bfc8a633686515
Thaianny Moraes <115707413+thaiannysarah@users.noreply.github.com>
2022-12-08 10:46:15 -0300

    Create parse.js

---
+++ parse.js
@@ -0,0 +1,11 @@
+var url = require('url');
+var adr = 'http://localhost:8080/index.html?ano=2022&mes=Dezembro';
+var q =url.parse(adr, true);
+
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); // index.html
+console.log(q.search); // ?ano=2022&mes=Dezembro
+console.log ('Brasil 2 x 1 Croácia')
+
+var qdata = q.query; // retorna como objeto ano: 2022 mes: dez
+console.log(qdata.mes) //'dezembro
